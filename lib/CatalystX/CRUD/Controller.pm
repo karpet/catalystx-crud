8d85312cb666f3f045fce68332451d4ad36913c2
Peter Karman <karpet@peknet.com>
2008-12-09 20:26:03 +0000

    use maybe::next::method shortcut

--- lib/CatalystX/CRUD/Controller.pm
+++ lib/CatalystX/CRUD/Controller.pm
@@ -107,7 +107,7 @@ Calls the form() method and saves the return value in stash() as C<form>.
 sub auto : Private {
     my ( $self, $c, @args ) = @_;
     $c->stash->{form} = $self->form($c);
-    $self->next::method( $c, @args ) if $self->next::can;
+    $self->maybe::next::method( $c, @args );
     1;
 }
 
@@ -716,7 +716,7 @@ sub form {
     elsif ( $self->{_form}->can('reset') ) {
         $self->{_form}->reset;
     }
-    $self->next::method($c) if $self->next::can;
+    $self->maybe::next::method($c);
     return $self->{_form};
 }
 
